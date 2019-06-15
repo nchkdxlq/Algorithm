8006d278766e63ee781328399698f908fbf1b7c6
luoquan <nchkdxlq.gmail.com>
2019-06-15 14:41:41 +0800

    update 387-firstUniqChar

--- Algorithm/LeetCode/387-firstUniqChar.cpp
+++ Algorithm/LeetCode/387-firstUniqChar.cpp
@@ -13,33 +13,35 @@
 
 using namespace std;
 
+typedef pair<int, int> IIPair;
+
 int firstUniqChar(string s) {
     int len = (int)s.length();
     if (len == 0) return -1;
     if (len == 1) return 0;
     
     // pair<int, int> 频次，第一次出现的index
-    unordered_map<char, pair<int, int>> mp;
+    unordered_map<char, IIPair> mp;
     // 统计字符出现的频次和第一次出现的下标
     for (int i = 0; i < len; i++) {
-        auto item = s[i];
-        int index = i;
-        int count = 1;
-        if (mp.find(item) != mp.end()) {  // 出现过
-            pair<int, int> p = mp[item];
-            count = ++p.first; // 频次加一
-            index = p.second;
+        if (mp.find(s[i]) != mp.end()) {  // 出现过
+#if 0
+            mp[s[i]].first++;
+#else
+            IIPair &p = mp[s[i]];
+            p.first++;
+#endif
+        } else {
+            auto newp = IIPair(1, i);
+            mp[s[i]] = newp;
         }
-        auto newp = make_pair(count, index);
-        mp[item] = newp;
     }
     
     int ret = -1;
     
     // 找到出现频次为1，最小的下标
     for (auto item : mp) {
-        pair<int, int> p = item.second;
-        cout << item.first << " " << p.first << " " << p.second << endl;
+        IIPair p = item.second;
         if (p.first == 1) {
             if (ret > -1) {
                 if (p.second < ret) {
@@ -58,6 +60,20 @@ int firstUniqChar(string s) {
 void __387_entry() {
     string str = "loveleetcode";
     int ret = firstUniqChar(str);
-    
     cout << "ret = " << ret << endl;
+    
+    unordered_map<int, int> mp;
+    mp[1] = 100;
+    
+    mp[1]++; //有效
+    
+    mp[1] += 1; //有效
+    
+    auto i = mp[1]; //无效 (坑)
+    i++;
+    
+    int &i2 = mp[1]; //有效
+    i2++;
+    
+    cout << mp[1] << endl;
 }
