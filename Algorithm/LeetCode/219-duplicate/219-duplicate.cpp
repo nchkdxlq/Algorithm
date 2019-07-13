0ef750bd10918360c9f621a8af9450d0546144af
luoquan <nchkdxlq.gmail.com>
2019-07-13 08:41:58 +0800

    add 219-duplicate -v1

--- Algorithm/LeetCode/219-duplicate/219-duplicate.cpp
+++ Algorithm/LeetCode/219-duplicate/219-duplicate.cpp
@@ -64,24 +64,80 @@ namespace duplicate_219 {
             return false;
         }
         
+        
+         bool v2_containsNearbyDuplicate(vector<int>& nums, int k) {
+             unordered_set<int> tmp_set;
+             
+             for (int i = 0; i < nums.size(); i++) {
+                 if (tmp_set.find(nums[i]) != tmp_set.end()) {
+                     return true;
+                 } else {
+                     tmp_set.insert(nums[i]);
+                     if (tmp_set.size() > k) {
+                         tmp_set.erase(nums[i-k]);
+                     }
+                 }
+             }
+             
+             return false;
+         }
+        
     public:
         bool containsNearbyDuplicate(vector<int>& nums, int k) {
-            return v1_containsNearbyDuplicate(nums, k);
+//            return v1_containsNearbyDuplicate(nums, k);
+            return v2_containsNearbyDuplicate(nums, k);
         }
     };
 }
 
 
+/**
+ 
+ # 解法一
+ 
+ ## 思路
+    使用hashMap记录不同元素出现的下标数组，然后再遍历map, 检查元素的下标数组中是否存在两个下标相差k
+ 
+ ## 算法
+    遍历数组，对每一个元素做如下操作
+      - 在map中存在，把下标添加到对应的vector中
+      - 在map中不存在，创建一个vector，把下标添加到vector中，建立元素与vector的映射关系
+ 
+ ## 复杂度分析
+    - 时间复杂度：O(n)遍历数组记录下标为O(n)， 遍历map也是O(n)，所以总体时间复杂度为O(n)
+    - 空间复杂度：O(n) 需要开辟额外的空间存储元素和下标的关系。
+ 
+ =================================================
+ 
+ # 解法二
+ 
+ ## 思路：用散列表来维护这个k大小的滑动窗口。
+ 
+ ## 算法：
+ 遍历数组，对于每个元素做以下操作：
+    - 在散列表中搜索当前元素，如果找到了就返回 true。
+    - 在散列表中插入当前元素。
+    - 如果当前散列表的大小超过了 k， 删除散列表中最旧的元素。
+ 返回 false。
+
+ ## 复杂度分析
+    - 时间复杂度：O(n) 我们会做 n 次 搜索，删除，插入 操作，每次操作都耗费常数时间。
+    - 空间复杂度：O(min(n, k)) 开辟的额外空间取决于散列表中存储的元素的个数，也就是滑动窗口的大小 O(min(n,k))。
+ 
+ */
 
 
 void __219_entry() {
     
+    // true
     vector<int> nums = {1,2,3,1};
     int k = 3;
     
+    // true
     nums = {1,0,1,1};
     k = 1;
-
+    
+    // false
     nums = {1,2,3,1,2,3};
     k = 2;
     
